4ced470b44562c828663a0529bafb5a60c0fca7d
Xinwei Ding <xind1cl@gmail.com>
2020-05-15 15:23:09 +1000

    able to query docs and save into classified db in batches

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 import { Response, Request, NextFunction } from "express";
-import db from "../db";
+import db, { nano } from "../db";
 
 /**
  * GET /api
@@ -40,33 +40,57 @@ export const langCount = async (req: Request, res: Response) => {
     res.json(data);
 };
 
+const handleDocs = async (db, docs: any[]) => {
+    return await Promise.all(docs.map(doc=> {
+        return db.insert({...doc.doc, _id: doc.id}).catch(console.log)
+    }));
+};
+
 export const stream = async (req: Request, res: Response) => {
+    const dbName = "tweets";
+    const classifiedDbName = `classified_${dbName}`;
+    await nano.db.get(classifiedDbName).catch(err => {
+        console.log(err);
+        nano.db.create(classifiedDbName).then(() => {
+            console.log("database created!");
+        });
+    });
+    const classifiedDb = nano.use(classifiedDbName);
+    const last = await classifiedDb.list({limit:10,descending: true}).then(body => body && body.rows && body.rows.find && body.rows.find(el => !el.id.startsWith("_") ));
+
     let run = true;
-    let startKey;
+    let startKey = last ? last.id  : null;
+    console.log(startKey);
     const params = {
         include_docs: true,
         limit: 10
-    }
-    console.log(startKey)
+    };
 
-
-    let i = 0
+    let i = 0;
     while(run)
     {
-        if(startKey) params.start_key_doc_id = startKey;
-        console.log(params)
-        await db.list(params)
+        if(startKey) {
+            params.start_key_doc_id = startKey;
+        }
+        const docs = await db.list(params)
           .then(body => {
               if(body.rows.length){
                   startKey = body.rows[body.rows.length -1].id;
-                  console.log(startKey);
               }
               else{
                   run = false;
               }
+
+
+
+              if(i > 0) run = false;
               i+=1;
 
+              return body.rows;
+
           });
+        if(docs)
+            await handleDocs(classifiedDb, docs)
     }
 
     res.json({});
